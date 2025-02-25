3ac5845f5b7826e7656bad98b4cedb03feb99a33
Artificial2003 <limbusurendra713@gmail.com>
2025-02-25 19:51:53 +0545

    Add files via upload

---
+++ Mad Libs game.py
@@ -0,0 +1,3 @@
+Holiday=input("Enter a off day: ")
+Person=input("Enter person name that the house belongs to: ")
+print("Today we are celebrating "+Holiday +" dinner with the whole family at "+Person +"'s House.")
